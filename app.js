fbe29a40aaa0cd51635a81e2f42c542dbbfb44b0
Soraya Rezaie <banorezaie2004@gmail.com>
2024-12-13 19:38:48 +0430

    new line

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+// inner loop to console * sign in colsole section 
 for(let i =0; i<10;i++){
-    for(let j=0; j<i;i++){
-        console.log("*");
+    for(let j=0; j<i;j++){
+        console.log("*\n");
     }
 }
